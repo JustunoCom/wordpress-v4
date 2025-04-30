b4ce4a5cbf7e413d646e0558b219cd8f7c291c2c
rizalishan <rizalishan@gmail.com>
2025-04-30 19:34:53 +0500

    ok

--- includes/js/admin.js
+++ includes/js/admin.js
@@ -1,6 +1,8 @@
 function justuno_generate_random_token() {
   const element = document.getElementById("ju4_justuno_woocommerce_token");
-  element.value =
-    Math.random().toString(36).substring(2, 24) +
-    Math.random().toString(36).substring(2, 24);
+  if (element && element !== null) {
+    element.value =
+      Math.random().toString(36).substring(2, 24) +
+      Math.random().toString(36).substring(2, 24);
+  }
 }
